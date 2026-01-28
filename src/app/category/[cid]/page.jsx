a47832d80e88bb3bc8371f440b1351da2399a4db
Manoj1001745 <khadkamanoj1001@gmail.com>
2026-01-28 12:32:54 +0545

    refactor Page component for improved readability, added mounted state check, and enhanced UI elements

--- src/app/category/[cid]/page.jsx
+++ src/app/category/[cid]/page.jsx
@@ -4,33 +4,39 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import { useParams } from 'next/navigation';
 
-function page() {
-    let {cid}=useParams()
-    let [post, setPost]=useState([])
-    useEffect(()=>{
+function Page() {
+    let {cid} = useParams()
+    let [post, setPost] = useState([])
+    let [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
         axios.get(`https://bankingkhabar.com/wp-json/wp/v2/posts?categories=${cid}`)
         .then(res => {
           setPost(res.data);
         });
-    },[cid])
-  return (
-    <section>
-   
-    <div className="container mx-auto">
-     <h3 className="text-2xl font-bold py-2"> </h3> 
-   <div className="box-22 flex gap-4 flex-wrap">
-   {post.map(a=>(
- <div className='box-33 p-3 w-[30%] box-1'>
-  <Link href={`/details/${a.id}`}>
-    <img className='h-[250px] object-cover' src={a.featured_image_src} alt="" />
-  <h1 className='pp-1 text-2xl text-red-600 font-bold py-3'>{a.title.rendered}</h1>
-  </Link>
-  </div>
-))}
- </div>
- </div>
-   </section>
-  )
+    }, [cid])
+
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <section>
+            <div className="container mx-auto">
+                <div className="box-22 flex gap-4 flex-wrap">
+                    {post.map(a => (
+                        <div key={a.id} className='box-33 p-3 w-[30%] box-1'>
+                            <Link href={`/details/${a.id}`}>
+                                <img className='h-[250px] object-cover' src={a.featured_image_src} alt={a.title?.rendered || 'Post image'} />
+                                <p className='pp-1 text-2xl text-red-600 hover:text-red-400 font-bold py-3'>{a.title?.rendered}</p>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
 }
 
-export default page
+export default Page
