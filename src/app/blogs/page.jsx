269cf7859ddbb6890903eddac38a63dd3a855d4f
Manoj1001745 <khadkamanoj1001@gmail.com>
2026-01-29 16:05:40 +0545

    Add BlogsPage component with metadata and render Blogs component

---
+++ src/app/blogs/page.jsx
@@ -0,0 +1,10 @@
+import Blogs from '../comp/Blogs';
+
+export const metadata = {
+    title: 'Blog - COL Global News',
+    description: 'Read the latest news, insights, and stories from around the world',
+};
+
+export default function BlogsPage() {
+    return <Blogs />;
+}
